6599cf1747cd6b8b017af1abc48225f069f88e0c
b_chirva <bchirva@gmail.com>
2026-08-04 17:04:12 +0500

    feat(dice chat action): add +/- modifiers

--- src/chat_actions/dice.py
+++ src/chat_actions/dice.py
@@ -5,7 +5,7 @@ from vkbottle.bot import Message
 
 from ..app import route
 
-DIE_REGEXP = re.compile("/(?P<count>[0-9]?)[dDkKдДкК](?P<edges>[0-9]+)$")
+DIE_REGEXP = re.compile("/(?P<count>[0-9]?)[dDkKдДкК](?P<edges>[0-9]+)?(?P<sign>[+-])?(?P<mod>[0-9]+)?$")
 
 
 @route.chat_message(regexp=DIE_REGEXP)
@@ -15,14 +15,28 @@ async def roll_die(message: Message):
         return
 
     count = int(match.group("count") or 1)
-    edges = int(match.group("edges"))
+    edges = int(match.group("edges") or 20)
+    sign = match.group("sign")
+    mod_raw = match.group("mod")
+    mod = 0
 
-    if count < 0 or edges < 0:
+    if sign and mod_raw is None:
+        sign = None
+
+
+    if sign and mod_raw is not None:
+        mod = int(mod_raw) if sign == '+' else -int(mod_raw)
+
+    if count <= 0 or edges <= 0:
         return
 
     result: str = f"🎲 Бросок кост{'ей' if count > 1 else 'и'} d{edges}: "
 
     for _ in range(count):
-        result += f"{random.randint(1, edges)} "
+        if (sign and mod_raw):
+            rand_die = random.randint(1, edges)
+            result += f"\n {rand_die + mod} ({rand_die}{sign}{mod_raw})"
+        else:
+            result += f"\n {random.randint(1, edges)} "
 
     await message.answer(result)
